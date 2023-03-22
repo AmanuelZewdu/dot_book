ff06ef1aff374a927dca9800c2fbc38f016834f2
Amanuel Z <azewdu@excellerentsolutions.com>
2023-03-22 13:29:48 +0300

    home page

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Home } from "./Components/Home";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Wrapper">
+      {/* <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav mr-auto">
+              <li>
+                <Link to={"/"} className="nav-link">
+                  {" "}
+                  Home{" "}
+                </Link>
+              </li>
+              <li>
+                <Link to={"/contact"} className="nav-link">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link to={"/about"} className="nav-link">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <hr />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router> */}
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter> */}
+      <header>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +57,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1>Library Management System</h1>
       </header>
+
+      <Navbar />
     </div>
   );
 }
